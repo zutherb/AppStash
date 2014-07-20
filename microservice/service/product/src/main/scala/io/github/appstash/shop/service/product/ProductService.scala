fe0486c1c43552bbda17b686c3598be918fbd7ab
Bernd Zuther <bernd.zuther@comsysto.com>
2014-07-20 13:30:04 +0200

    add google product xml feed

--- microservice/service/product/src/main/scala/io/github/appstash/shop/service/product/ProductService.scala
+++ microservice/service/product/src/main/scala/io/github/appstash/shop/service/product/ProductService.scala
@@ -1,8 +1,8 @@
 package io.github.appstash.shop.service.product
 
 import akka.actor.Actor
-import reactivemongo.api.MongoDriver
-import spray.http.MediaTypes
+import akka.util.Helpers.Requiring
+import spray.http.{StatusCodes, HttpResponse, MediaTypes}
 import spray.httpx.SprayJsonSupport.sprayJsonMarshaller
 import spray.httpx.SprayJsonSupport.sprayJsonUnmarshaller
 import spray.json.pimpAny
@@ -28,26 +28,59 @@ class ProductServiceActor extends Actor with ProductService {
 // this trait defines our service behavior independently from the service actor
 trait ProductService extends HttpService {
   val myRoute =
-    path("all") {
-      get {
-        respondWithMediaType(MediaTypes.`application/json`) {
-          complete {
-            Products.findAll()
-          }
+  path("all") {
+    get {
+      respondWithMediaType(MediaTypes.`application/json`) {
+        complete {
+          Products.findAll()
         }
       }
+    }
     } ~
-      path("search") {
-        get {
-          parameter("productType".?, "id".?, "articleId".?, "name".?, "urlname".?){
-            (productType, id, articleId, name, urlname) =>
-            respondWithMediaType(MediaTypes.`application/json`) {
-              complete {
-                val query: ProductQuery = new ProductQuery(id, productType, articleId, name, urlname)
-                Products.findByQuery(query)
+  path("search") {
+    get {
+      parameter("productType".?, "id".?, "articleId".?, "name".?, "urlname".?) {
+        (productType, id, articleId, name, urlname) =>
+          respondWithMediaType(MediaTypes.`application/json`) {
+            complete {
+              val query: ProductQuery = new ProductQuery(id, productType, articleId, name, urlname)
+              Products.findByQuery(query)
+            }
+          }
+      }
+    }
+  } ~
+    path("feed") {
+      get {
+        respondWithMediaType(MediaTypes.`application/xml`) {
+          // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+            val products = Products.findAll()
+            products map { products =>
+              products match {
+                case _ =>
+                  <rss version="2.0" xmlns:g="http://base.google.com/ns/1.0">
+                    <channel>
+                      <title>Name Ihres Daten-Feeds</title>
+                      <link>http://www.example.com</link>
+                      <description>Beschreibung des Inhalts</description>{for (product <- products.value) yield
+                      <item>
+                        <title>
+                          {product.name}
+                        </title>
+                        <link>http://www.example.com/item1-info-page.html</link>
+                        <description>
+                          {product.description}
+                        </description>
+                        <g:image_link>http://www.example.com/image1.jpg</g:image_link> <g:price>25</g:price> <g:condition>neu</g:condition> <g:id>1a</g:id>
+                      </item>}
+                    </channel>
+                  </rss>
+//                case _ => HttpResponse(StatusCodes.BadRequest)
               }
             }
           }
+        }
       }
     }
 }
