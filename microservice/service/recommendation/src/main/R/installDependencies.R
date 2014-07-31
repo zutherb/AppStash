07a7a48d6d8ac43bce3b8e0b8f0d8b43bf6cf6eb
Bernd Zuther <bernd.zuther@codecentric.de>
2014-07-31 18:26:32 +0200

    fix recommendation service build

--- microservice/service/recommendation/src/main/R/installDependencies.R
+++ microservice/service/recommendation/src/main/R/installDependencies.R
@@ -1,7 +1,3 @@
 install.packages("arules", repos="http://cran.fhcrc.org", dependencies = TRUE)
 install.packages("recommenderlab", repos="http://cran.fhcrc.org", dependencies = TRUE)
 install.packages("rjson", repos="http://cran.fhcrc.org", dependencies = TRUE)
-
-library("arules")
-library("recommenderlab")
-library("rjson")
