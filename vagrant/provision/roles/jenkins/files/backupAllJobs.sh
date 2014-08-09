a8db947a749211ffca303075b2e72d8d1b6ed887
Bernd Zuther <bernd.zuther@comsysto.com>
2014-08-09 16:42:36 +0200

    fix jobs xml and solve update / create problem during provisioning

--- vagrant/provision/roles/jenkins/files/backupAllJobs.sh
+++ vagrant/provision/roles/jenkins/files/backupAllJobs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 cp -f /var/lib/jenkins/config.xml /provision/roles/jenkins/files/config.xml
 
+echo "Download Jenkins Cli"
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar -O /tmp/jenkins-cli.jar
-for BUILD in $(java -jar /tmp/jenkins-cli.jar -s http://localhost:8080 list-jobs)
+
+echo "save Jobs"
+java -jar /tmp/jenkins-cli.jar -s http://localhost:8080 list-jobs &> /provision/roles/jenkins/files/jobs.txt
+for BUILD in $(cat /provision/roles/jenkins/files/jobs.txt)
 do
 	java -jar /tmp/jenkins-cli.jar -s http://localhost:8080 get-job $BUILD &> /provision/roles/jenkins/files/$BUILD.xml
 done
