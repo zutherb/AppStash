9b367b5944b2ce1fc255d0be67cc0908cd9abc59
Bernd Zuther <bernd.zuther@codecentric.de>
2014-08-09 11:48:47 +0200

    provisioning ob the whole cluster should work

---
+++ vagrant/provision/roles/tomcat/files/logback.groovy
@@ -0,0 +1,31 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.FileAppender
+import net.logstash.logback.encoder.LogstashEncoder
+
+import static ch.qos.logback.classic.Level.ERROR
+import static ch.qos.logback.classic.Level.INFO
+
+scan()
+
+appender("stdout", ConsoleAppender) {
+    encoder(LogstashEncoder) {
+    }
+}
+
+appender("appstash", FileAppender) {
+    file = "/var/log/tomcat6/appstash.log"
+    append = true
+    encoder(PatternLayoutEncoder) {
+        pattern = "%msg%n"
+    }
+}
+
+logger("org.apache.wicket", INFO)
+logger("org.springframework.web.client", ERROR)
+logger("org.springframework", INFO)
+logger("io.github.appstash", INFO)
+logger("io.github.appstash.shop.service.recommendation.impl", INFO)
+logger("io.github.appstash", INFO, ["appstash"], false)
+
+root(ERROR, ["stdout", "appstash"])
