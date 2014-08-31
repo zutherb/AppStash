840674780df6c6db8d1804ce66f89a04aab15bb6
Bernd Zuther <bernd.zuther@codecentric.de>
2014-08-31 12:44:47 +0200

    product item directive now has its own scope

--- microservice/frontend/app/scripts/directives/productItem.ts
+++ microservice/frontend/app/scripts/directives/productItem.ts
@@ -1,12 +1,13 @@
 eshop.directive("shopProductItem", function(){
-    scope: true;
-
     var directive: ng.IDirective = {};
 
     directive.restrict = "AE";
     directive.templateUrl = "/partials/productItem.html";
     directive.replace = true;
-
+    directive.scope = {
+        product: '=product'
+    }
+    console.log(directive)
     return directive;
 });
 
