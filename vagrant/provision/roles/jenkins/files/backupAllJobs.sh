bc686a8324669fe4f9e5cd33acb8126003bbb33d
Bernd Zuther <bernd.zuther@codecentric.de>
2014-09-06 10:14:19 +0200

    fix ui build

--- vagrant/provision/roles/jenkins/files/backupAllJobs.sh
+++ vagrant/provision/roles/jenkins/files/backupAllJobs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+echo "Copy Jenkins Configuration"
 cp -f /var/lib/jenkins/config.xml /provision/roles/jenkins/files/config.xml
 
 echo "Download Jenkins Cli"
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar -O /tmp/jenkins-cli.jar
 
-echo "save Jobs"
+echo "Save Jenkins Jobs"
 java -jar /tmp/jenkins-cli.jar -s http://localhost:8080 list-jobs &> /provision/roles/jenkins/files/jobs.txt
 for BUILD in $(cat /provision/roles/jenkins/files/jobs.txt)
 do
