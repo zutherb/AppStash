2d4455569382300f521d683f3487756a1fb3c05d
Bernd Zuther <bernd.zuther@codecentric.de>
2014-09-14 14:49:34 +0200

    add navigation build scripts and config

--- microservice/service/navigation/src/main/java/io/github/appstash/shop/service/navigation/AppModule.java
+++ microservice/service/navigation/src/main/java/io/github/appstash/shop/service/navigation/AppModule.java
@@ -25,56 +25,17 @@ public class AppModule {
     @Provides
     @Named("restx.admin.password")
     public String restxAdminPassword() {
-        return "5327";
+        return "admin123";
     }
 
     @Provides @Named(MongoModule.MONGO_DB_NAME)
     public String dbName() {
-        return "shop";
+        return System.getProperty("mongo.db", "shop");
     }
 
     @Provides
     public ConfigSupplier appConfigSupplier(ConfigLoader configLoader) {
         // Load settings.properties in io.github.appstash.shop.service.navigation package as a set of config entries
         return configLoader.fromResource("io/github/appstash/shop/service/navigation/settings");
-    }
-
-    @Provides
-    public CredentialsStrategy credentialsStrategy() {
-        return new BCryptCredentialsStrategy();
-    }
-
-    @Provides
-    public BasicPrincipalAuthenticator basicPrincipalAuthenticator(
-            SecuritySettings securitySettings, CredentialsStrategy credentialsStrategy,
-            @Named("restx.admin.passwordHash") String defaultAdminPasswordHash, ObjectMapper mapper) {
-        return new StdBasicPrincipalAuthenticator(new StdUserService<>(
-                // use file based users repository.
-                // Developer's note: prefer another storage mechanism for your users if you need real user management
-                // and better perf
-                new FileBasedUserRepository<>(
-                        StdUser.class, // this is the class for the User objects, that you can get in your app code
-                        // with RestxSession.current().getPrincipal().get()
-                        // it can be a custom user class, it just need to be json deserializable
-                        mapper,
-
-                        // this is the default restx admin, useful to access the restx admin console.
-                        // if one user with restx-admin role is defined in the repository, this default user won't be
-                        // available anymore
-                        new StdUser("admin", ImmutableSet.<String>of("*")),
-
-                        // the path where users are stored
-                        Paths.get("data/users.json"),
-
-                        // the path where credentials are stored. isolating both is a good practice in terms of security
-                        // it is strongly recommended to follow this approach even if you use your own repository
-                        Paths.get("data/credentials.json"),
-
-                        // tells that we want to reload the files dynamically if they are touched.
-                        // this has a performance impact, if you know your users / credentials never change without a
-                        // restart you can disable this to get better perfs
-                        true),
-                credentialsStrategy, defaultAdminPasswordHash),
-                securitySettings);
     }
 }
