a76ad6f3f490ec1a10488cc0800bfe8554951465
Bernd Zuther <bernd.zuther@codecentric.de>
2014-09-20 09:42:52 +0200

    fix version page

--- monolithic/ui/src/main/java/io/github/appstash/shop/ui/page/VersionPage.java
+++ monolithic/ui/src/main/java/io/github/appstash/shop/ui/page/VersionPage.java
@@ -1,41 +1,48 @@
 package io.github.appstash.shop.ui.page;
 
+import io.github.appstash.shop.ui.application.ShopApplication;
 import net.logstash.logback.encoder.org.apache.commons.io.FileUtils;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.request.Response;
-import org.apache.wicket.response.StringResponse;
+import org.apache.wicket.request.http.WebResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
 import org.wicketstuff.annotation.mount.MountPath;
 
-import java.io.IOException;
+import java.io.File;
 
 @MountPath("/version")
 public class VersionPage extends WebPage {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(VersionPage.class);
-
-    public static final ClassPathResource CLASS_PATH_RESOURCE = new ClassPathResource("/META-INF/MANIFEST.MF");
+    private static final String MANIFEST_MF_PATH = "/META-INF/MANIFEST.MF";
 
     public VersionPage() {
-        Response response = newStringResponse();
-        getRequestCycle().setResponse(response);
     }
 
-    private Response newStringResponse() {
-        Response response = new StringResponse();
+    private WebResponse newStringResponse() {
+        WebResponse response = (WebResponse) getResponse();
+        response.setContentType("text/plain");
         response.write(responseText());
+        response.flush();
+        response.setStatus(HttpStatus.OK.value());
         return response;
     }
 
     private String responseText() {
-        try{
-            return FileUtils.readFileToString(CLASS_PATH_RESOURCE.getFile());
-        }catch (Exception e){
+        try {
+            String realPath = ShopApplication.get().getServletContext().getRealPath(MANIFEST_MF_PATH);
+            return FileUtils.readFileToString(new File(realPath));
+        } catch (Exception e) {
             LOGGER.error("Manifast could not be opened: ", e);
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public void renderPage() {
+        WebResponse response = newStringResponse();
+        getRequestCycle().setResponse(response);
     }
 
 
