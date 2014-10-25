e77146b238e94afb2730b408db4790315260f1be
Bernd Zuther <bernd.zuther@codecentric.de>
2014-10-25 17:51:56 +0200

    add profiles to cart

--- microservice/service/cart/src/main/groovy/io/github/appstash/shop/service/cart/config.groovy
+++ microservice/service/cart/src/main/groovy/io/github/appstash/shop/service/cart/config.groovy
@@ -1,10 +1,12 @@
 package io.github.appstash.shop.service.cart
 
 import io.github.appstash.shop.service.cart.domain.CartItem
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
@@ -17,9 +19,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @ComponentScan
 class ApplicationConfiguration extends WebMvcConfigurationSupport {
 
+    @Autowired
+    Environment environment;
+
     @Bean
     RedisConnectionFactory connectionFactory() {
-        new JedisConnectionFactory()
+        def factory = new JedisConnectionFactory();
+        factory.setHostName(environment.getProperty("cart.service.redis.url","localhost"))
+        factory
     }
 
     @Bean
