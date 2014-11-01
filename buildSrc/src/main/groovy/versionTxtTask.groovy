1d289712bf7ec7c6716a8c757d85a2f1820823de
Bernd Zuther <bernd.zuther@codecentric.de>
2014-11-01 20:35:30 +0100

    clean up build script

---
+++ buildSrc/src/main/groovy/versionTxtTask.groovy
@@ -0,0 +1,18 @@
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.Input
+import org.gradle.api.java.archives.Manifest
+
+class VersionTxtTask extends DefaultTask {
+
+    @Input
+    Manifest manifest
+
+    @Input
+    String filename
+
+    @TaskAction
+    def create() {
+        manifest.writeTo(filename)
+    }
+}
