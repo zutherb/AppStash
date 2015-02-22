5dab07972d277c4ad49698ffc28f252a83e15c71
Bernd Zuther <bernd.zuther@codecentric.de>
2015-02-22 08:49:17 +0100

    add kubernetes service definitions

--- kubernetes/start_cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-../../kubernetes/cluster/kubectl.sh create -f redis.json
-../../kubernetes/cluster/kubectl.sh create -f cart.json
-../../kubernetes/cluster/kubectl.sh create -f mongodb.json
-../../kubernetes/cluster/kubectl.sh create -f product.json
-../../kubernetes/cluster/kubectl.sh create -f navigation.json
-../../kubernetes/cluster/kubectl.sh create -f shop.json
-../../kubernetes/cluster/kubectl.sh create -f catalog.json
