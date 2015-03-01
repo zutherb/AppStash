a1985636d3589c6720affecc3babe1ae1a2baddd
Bernd Zuther <bernd.zuther@codecentric.de>
2015-03-01 22:14:57 +0100

    update jedis driver and add pooling

--- microservice/service/cart/src/main/groovy/io/github/zutherb/appstash/shop/service/cart/config.groovy
+++ microservice/service/cart/src/main/groovy/io/github/zutherb/appstash/shop/service/cart/config.groovy
@@ -17,6 +17,8 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.springframework.web.filter.ShallowEtagHeaderFilter
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
+import redis.clients.jedis.JedisPoolConfig
+import redis.clients.jedis.Protocol
 
 @Configuration
 @EnableAutoConfiguration
@@ -30,11 +32,27 @@ class ApplicationConfiguration extends WebMvcConfigurationSupport {
 
     @Bean
     RedisConnectionFactory connectionFactory() {
-        def factory = new JedisConnectionFactory();
-        def hostName = environment.getProperty("CART_SERVICE_REDIS_URL", environment.getProperty("REDIS_PORT_6379_TCP_ADDR", "localhost"))
-        LOGGER.info("Redis Hostname is $hostName")
+        def factory = new JedisConnectionFactory(poolConfig());
+        def hostName = environment.getProperty("CART_SERVICE_REDIS_URL",
+                environment.getProperty("REDIS_PORT_6379_TCP_ADDR", "localhost"))
+        def port = environment.getProperty("REDIS_PORT_6379_TCP_PORT", Integer.class, Protocol.DEFAULT_PORT)
+        LOGGER.info("Redis Hostname is $hostName:$port")
         factory.setHostName(hostName)
+        factory.setPort(port)
+        factory.setUsePool(true)
         factory
+    }
+
+    @Bean
+    JedisPoolConfig poolConfig() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(10000);
+        poolConfig.setMinIdle(1000);
+        poolConfig.setMaxIdle(-1);
+        poolConfig.setMaxWaitMillis(500);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig
     }
 
     @Bean
@@ -52,6 +70,6 @@ class ApplicationConfiguration extends WebMvcConfigurationSupport {
         ShallowEtagHeaderFilter etagHeaderFilter = new ShallowEtagHeaderFilter();
         registrationBean.setFilter(etagHeaderFilter);
         registrationBean.setOrder(1);
-        return registrationBean;
+        registrationBean;
     }
 }
