fd297814effec16cc435e159eeeb87005857c47b
Bernd Zuther <bernd.zuther@codecentric.de>
2015-06-13 20:25:06 +0200

    fix typos

--- slides/09_devoxx_pl/slide_config.js
+++ slides/09_devoxx_pl/slide_config.js
@@ -1,2 +1,2 @@
 SLIDE_CONFIG.settings.title = 'Microservices and Conversion Hunting';
-SLIDE_CONFIG.settings.subtitle = 'How to build software architectures for changeableness';
+SLIDE_CONFIG.settings.subtitle = 'How to build software architectures for adaptability';
