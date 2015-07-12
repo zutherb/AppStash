f30a314dd7d96bd2556b08257e2b44acadcd5388
Bernd Zuther <bernd.zuther@codecentric.de>
2015-07-12 22:53:44 +0200

    - update kubernetes version

--- kubernetes/init_cluster.sh
+++ kubernetes/init_cluster.sh
@@ -3,7 +3,7 @@ brew install kubernetes-cli wget fleetctl etcdctl
 git submodule init
 git submodule update
 cd ./vagrant/
-NODE_MEM=1536 NODE_CPUS=1 NUM_INSTANCES=3 vagrant up
+KUBERNETES_VERSION=0.21.0 NODE_MEM=1536 NODE_CPUS=1 NUM_INSTANCES=3 vagrant up
 cd ../example/shop/
 ./deploy_cluster.sh
 cd ../..
