98eb7f47581664659737e174afd7e945388e654d
Bernd Zuther <bernd.zuther@codecentric.de>
2015-07-12 10:19:56 +0200

    rename script

--- vamp/downgrade_shop_application.sh
+++ vamp/downgrade_shop_application.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-curl -v -X POST --data-binary @shop.yml -H "Content-Type: application/x-yaml" http://$(boot2docker ip):8081/api/v1/deployments
+export BOOT2DOCKER_URL=http://$(boot2docker ip)
+export APP_NAME=$(curl -s $BOOT2DOCKER_URL:8081/api/v1/deployments | jq '.[0].name' | sed -e 's/^"//'  -e 's/"$//')
+echo "APP_NAME=$APP_NAME"
+
+curl -v -X PUT --data-binary @shop.yml -H "Content-Type: application/x-yaml" http://$(boot2docker ip):8081/api/v1/deployments/$APP_NAME
