453c909c1cf86d7b8f15edf8652cad9c1b7bff87
Bernd Zuther <bernd.zuther@codecentric.de>
2015-07-13 22:21:42 +0200

    remove downgrade script

--- vamp/downgrade_shop_application.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export BOOT2DOCKER_URL=http://$(boot2docker ip)
-export APP_NAME=$(curl -s $BOOT2DOCKER_URL:8081/api/v1/deployments | jq '.[0].name' | sed -e 's/^"//'  -e 's/"$//')
-echo "APP_NAME=$APP_NAME"
-
-curl -v -X PUT --data-binary @shop.yml -H "Content-Type: application/x-yaml" http://$(boot2docker ip):8081/api/v1/deployments/$APP_NAME
-sed 's/__APP_NAME__/$APP_NAME/g' remove_variant_b.tpl.yml | \
-    curl -v -X DELETE --data-binary @- -H "Content-Type: application/x-yaml" http://$(boot2docker ip):8081/api/v1/deployments/$APP_NAME
