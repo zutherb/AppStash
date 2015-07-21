2b2eb75387ee862679540284a3afc88ce5b0cf36
Bernd Zuther <bernd.zuther@codecentric.de>
2015-07-21 22:33:19 +0200

    small improvements

---
+++ vamp/launch_canary_release.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+export BOOT2DOCKER_URL=http://$(boot2docker ip)
+export APP_NAME=$(curl -s $BOOT2DOCKER_URL:8081/api/v1/deployments | jq '.[0].name' | sed -e 's/^"//'  -e 's/"$//')
+echo "APP_NAME=$APP_NAME"
+
+curl -v -X PUT --data-binary @shop-canary-release-test.yml -H "Content-Type: application/x-yaml" $BOOT2DOCKER_URL:8081/api/v1/deployments/$APP_NAME
