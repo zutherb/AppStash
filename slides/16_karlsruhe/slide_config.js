ee0b7ca39b852536602b75ebee617792f768c68a
Bernd Zuther <bernd.zuther@codecentric.de>
2015-12-23 10:52:34 +0100

    add slides

--- slides/16_karlsruhe/slide_config.js
+++ slides/16_karlsruhe/slide_config.js
@@ -1,2 +1,2 @@
-SLIDE_CONFIG.settings.title = 'Microservice-Deployment ganz einfach';
-SLIDE_CONFIG.settings.subtitle = '';
+SLIDE_CONFIG.settings.title = 'Von Null auf Hundert mit Microservices';
+SLIDE_CONFIG.settings.subtitle = 'DevOps Karlsruhe Meetup - 28. Jan 2016';
